e37e97cabbc20cc6d7cf92c23219e68fce43168b
jraikisto <Raikisto.Jaakko.N@student.uta.fi>
2018-04-17 01:11:35 +0300

    lets see if numbers work

--- main.py
+++ main.py
@@ -5,16 +5,22 @@ from random import randint
 import unicornhat as unicorn
 from weather import Weather
 from pics import Pics
+import math
 
 unicorn.set_layout(unicorn.AUTO)
 unicorn.rotation(180)
 unicorn.brightness(0.175)
 uh_width,uh_height=unicorn.get_shape()
 
-def drawPic(pic):
+def drawPic(pics):
+    pic1 = pics[0]
+    pic2 = pics[1]
     for h in range(uh_height):
         for w in range(uh_width):
-            unicorn.set_pixel(w, h, pic[h][w][0], pic[h][w][1], pic[h][w][2])
+            if w < 5:
+                unicorn.set_pixel(w, h, pic1[h][w][0], pic1[h][w][1], pic1[h][w][2])
+            else:
+                unicorn.set_pixel(w, h, pic2[h][w][0], pic2[h][w][1], pic2[h][w][2])
     unicorn.show()
 
 def weatherUpdate():
@@ -23,7 +29,11 @@ def weatherUpdate():
     condition = location.condition()
     return condition
 
-wet = weatherUpdate()
+nums = [Pics.one, Pics.two, Pics.three]
 while True:
-    drawPic(Pics.one)
-    sleep(2)
+    for n in nums:
+        for x in range(0, 99)
+            wet = weatherUpdate()
+            print(wet.text())
+            drawPic(retNum(x, 255, 255, 255))
+            sleep(1)
